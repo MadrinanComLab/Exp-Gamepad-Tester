02777c6ba9b895fa49617481b79a3b755c24309d
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2024-01-31 13:22:02 +0800

    Handing of the controller sticks was created

--- index.js
+++ index.js
@@ -34,10 +34,32 @@ handleButtons = (buttons) => {
     }
 }
 
+handleSticks = (axes) => {
+    /* This will detect the movement in left stick and the axes[0] is for the left and right. while axes[1] is for detecting up and down. */
+    updateStick("controller-b10", axes[0], axes[1]);
+    
+    /* This will detect the movement in right stick and the axes[2] is for the left and right. while axes[3] is for detecting up and down. */
+    updateStick("controller-b11", axes[2], axes[3]);
+}
+
+updateStick = (element_id, left_right_axis, up_down_axis) => {
+    const multiplier = 25;
+    const stick_left_right = left_right_axis * multiplier;
+    const stick_up_down = up_down_axis * multiplier;
+
+    const stick = document.getElementById(element_id);
+    const x = Number(stick.dataset.originalXPosition);
+    const y = Number(stick.dataset.originalYPosition);
+
+    stick.setAttribute("cx", x + stick_left_right);
+    stick.setAttribute("cy", y + stick_up_down);
+}
+
 gameLoop = () => {
     if(controller_index !== null){
         const gamepad = navigator.getGamepads()[controller_index];
         handleButtons(gamepad.buttons);
+        handleSticks(gamepad.axes);
     }
     requestAnimationFrame(gameLoop);
 }
